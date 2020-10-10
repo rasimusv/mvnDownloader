13d773d655ed968155eeb48401f1359920c4d4f6
rasimusv <rasimusv@gmail.com>
2020-10-10 23:51:50 +0300

    Minor changes

---
+++ src/main/java/ru/itis/rasimusv/mvnDownloader/Main.java
@@ -0,0 +1,10 @@
+package ru.itis.rasimusv.mvnDownloader;
+
+import ru.itis.rasimusv.MultiThreadDownloader.app.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Program.main(args);
+    }
+}
